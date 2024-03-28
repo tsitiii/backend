d80062d096964c1021898ac49535e5cc9c36f9c1
Ludis-et <leulsegedmelaku1020@gmail.com>
2024-03-28 10:20:43 +0300

    adding some libraries

--- backend/settings.py
+++ backend/settings.py
@@ -32,6 +32,7 @@ INSTALLED_APPS = [
 
     'rest_framework',
     'djoser',
+    'corsheaders',
 
     'core',
     'post',
@@ -42,6 +43,9 @@ AUTH_USER_MODEL = 'user.User'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+
+    'corsheaders.middleware.CorsMiddleware',
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -84,7 +88,11 @@ SIMPLE_JWT = {
 DJOSER = {
     'SERIALIZERS':{
         'user_create': 'user.serializers.UserCreateSerializer'
-    }
+    },
+    'PASSWORD_RESET_CONFIRM_URL': '#/password/reset/confirm/{uid}/{token}',
+    'USERNAME_RESET_CONFIRM_URL': '#/username/reset/confirm/{uid}/{token}',
+    'ACTIVATION_URL': '#/activate/{uid}/{token}',
+    'SEND_ACTIVATION_EMAIL': True,
 }
 
 
@@ -143,3 +151,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOW_ALL_ORIGINS = True
